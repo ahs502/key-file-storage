e1a7c91cda54e06decb61bd1df31ba8be1c77d47
Hessamoddin A Shokravi <hessam@ahs502.ir>
2019-08-09 02:11:57 +0430

    Minor fixes on collection query cache.

--- src/create-cache.ts
+++ src/create-cache.ts
@@ -1,9 +1,7 @@
 export default function createCache(cacheConfig?: number | boolean) {
   if (cacheConfig === true || typeof cacheConfig === 'undefined') {
     // Unlimited cache by default
-    return {
-      /*CACHE*/
-    };
+    return createCache_Unlimited(cacheConfig);
   } else if (cacheConfig === false) {
     // No cache
     return createCache_NoCache(cacheConfig);
@@ -12,6 +10,45 @@ export default function createCache(cacheConfig?: number | boolean) {
     return createCache_LimitedByKeyCount(cacheConfig);
   } else {
     throw new Error('Invalid cache config.');
+  }
+
+  function createCache_Unlimited(cacheConfig: true | undefined) {
+    return new Proxy(
+      {
+        /*CACHE*/
+      },
+      {
+        set: function(target, property, value, receiver) {
+          property in target || cleanUpCachedCollections(target, property);
+          return ((target as any)[property] = value);
+        },
+
+        get: function(target, property, receiver) {
+          return (target as any)[property];
+        },
+
+        deleteProperty: function(target, property) {
+          if (property === '*') {
+            Object.keys(target).forEach(key => delete (target as any)[key]);
+            return true;
+          }
+          property in target && cleanUpCachedCollections(target, property);
+          return delete (target as any)[property];
+        },
+
+        has: function(target, property) {
+          return property in target;
+        },
+      },
+    );
+
+    function cleanUpCachedCollections(target: any, property: string | number | symbol) {
+      const path = String(property);
+      if (path.slice(-1) === '/') return;
+      const parts = path.split('/');
+      const collections = parts.map((part, index) => parts.slice(0, index).join('/') + '/');
+      collections.forEach(collection => delete target[collection]);
+    }
   }
 
   function createCache_NoCache(cacheConfig: false) {
@@ -51,6 +88,14 @@ export default function createCache(cacheConfig?: number | boolean) {
       },
       {
         set: function(target, property, value, receiver) {
+          if (!keys.includes(property)) {
+            keys
+              .filter(key => key === '/' || (key.slice(-1) === '/' && String(property).indexOf(key) === 0))
+              .forEach(key => {
+                updateKeys(target, key, 'DELETE');
+                delete (target as any)[key];
+              });
+          }
           updateKeys(target, property, 'SET');
           return ((target as any)[property] = value);
         },
@@ -65,6 +110,14 @@ export default function createCache(cacheConfig?: number | boolean) {
             keys = Array(keyNumber);
             nextKeyIndex = 0;
             return true;
+          }
+          if (keys.includes(property)) {
+            keys
+              .filter(key => key === '/' || (key.slice(-1) === '/' && String(property).indexOf(key) === 0))
+              .forEach(key => {
+                updateKeys(target, key, 'DELETE');
+                delete (target as any)[key];
+              });
           }
           updateKeys(target, property, 'DELETE');
           return delete (target as any)[property];
